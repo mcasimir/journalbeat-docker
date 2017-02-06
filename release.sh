a7b094a1aaaf4266f29e74116cfb713d76d91d04
Maurizio Casimirri <maurizio.cas@gmail.com>
2017-02-06 22:06:08 +0100

    Makefile and readme

--- release.sh
+++
@@ -1,8 +0,0 @@
-docker build -t mcasimir/journalbeat .
-versionString=$(docker run mcasimir/journalbeat /bin/journalbeat-debian --version)
-version=$(echo $versionString) | awk '{print $3}'
-
-echo version
-
-# docker tag mcasimir/journalbeat mcasimir/journalbeat:latest
-# docker tag mcasimir/journalbeat mcasimir/journalbeat:"$version"
